49b6f1819a0a795604a7cf7ed087c4f010bd4820
I Joo Han <juhanlee2218@gmail.com>
2023-06-06 17:31:27 +0900

    Feat : user 회원가입 로그인 기능 추가 user 회원가입과 로그인 기능 추가 Feat #4

---
+++ users/serializers.py
@@ -0,0 +1,97 @@
+from rest_framework import serializers
+from users.models import User
+from .models import User, password_validator, password_pattern
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# 회원가입에 필요한 serializer
+class SignUpSerializer(serializers.ModelSerializer):
+    '''
+    작성자 : 이주한
+    내용 : 회원가입에 필요한 Sign Up Serializer 클래스
+    최초 작성일 : 2023.06.06
+    업데이트 일자 :
+    '''
+    re_password = serializers.CharField(
+        error_messages={
+            "required": "비밀번호 확인은 필수 입력 사항입니다!",
+            "blank": "비밀번호 확인은 필수 입력 사항입니다!",
+            "write_only": True,
+        }
+    )
+    
+    class Meta:
+        model = User
+        fields = (
+            "name",
+            "password",
+            "re_password",
+            "email",
+        )
+        extra_kwargs = {
+            "email": {
+                "error_messages": {
+                    "required": "email은 필수 입력 사항입니다!",
+                    "invalid": "email 형식이 맞지 않습니다. 알맞은 형식의 email을 입력해주세요!",
+                    "blank": "email은 필수 입력 사항입니다!",
+                }
+            },
+            "name": {
+                "error_messages": {
+                    "required": "이름은 필수 입력 사항입니다!",
+                    "blank": "이름은 필수 입력 사항입니다!",
+                }
+            },
+            "password": {
+                "write_only": True,
+                "error_messages": {
+                    "required": "비밀번호는 필수 입력 사항입니다!",
+                    "blank": "비밀번호는 필수 입력 사항입니다!",
+                },
+            },
+        }
+
+    def validate(self, data):
+        password = data.get("password")
+        re_password = data.get("re_password")
+
+        # 비밀번호 & 비밀번호 확인 일치 검사
+        if password != re_password:
+            raise serializers.ValidationError(detail={"password": "비밀번호와 비밀번호 확인이 일치하지 않습니다!"})
+
+        # 비밀번호 유효성 검사
+        if password_validator(password):
+            raise serializers.ValidationError(detail={"password": "비밀번호는 8자 이상의 영문 대소문자와 숫자, 특수문자를 포함하여야 합니다!"})
+
+        # 비밀번호 유효성 검사
+        if password_pattern(password):
+            raise serializers.ValidationError(detail={"password": "비밀번호는 연속해서 3자리 이상 동일한 영문,숫자,특수문자 사용이 불가합니다!"})
+
+        return data
+
+    def create(self, validated_data):
+        email = validated_data["email"]
+        name = validated_data["name"]
+        password = validated_data["password"]
+        # 새로운 User 인스턴스를 생성과정에서 re_password가 예외를 발생시킬 수 있기 때문에 create() 메소드에서 check_password 필드를 제거합니다.
+        validated_data.pop("re_password", None)
+        user = User.objects.create(name=name, email=email,)
+        user.set_password(password)
+        user.save()
+
+        return validated_data
+
+
+# 로그인시 필요한 JWT 토큰 serializer
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    '''
+    작성자 : 이주한
+    내용 : 로그인에 필요한 Custom Token Obtain Pair Serializer 클래스
+    최초 작성일 : 2023.06.06
+    업데이트 일자 :
+    '''
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['name'] = user.name
+        token["is_admin"] = user.is_admin
+        return token
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from users import views
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-urlpatterns = [    
-    path('main/',  TokenObtainPairView.as_view(), name='main'),
+urlpatterns = [
+    # 회원가입
+    path('signup/', views.UserView.as_view(), name='sign_up'),
+    
+    # 로그인
+    path('login/', TokenObtainPairView.as_view(), name='log_in'),
+    # path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
