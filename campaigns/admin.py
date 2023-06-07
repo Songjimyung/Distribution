4fc062f914465330122d7a6480c43723bd1ac178
Choi Junyoung <nueeeng@gmail.com>
2023-06-07 19:16:59 +0900

    Feat : Campaign, Funding admin페이지 추가
    Campaign, Funding admin페이지 추가하였습니다. 추후 Review, Comment, FundingOrder까지
    추가 예정입니다.
    Issue번호 : #37
    
    Feat #37

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -9,11 +9,11 @@ from campaigns.models import (
 
 
 @admin.register(Campaign)
-class ReviewDisplay(admin.ModelAdmin):
+class CampaignDisplay(admin.ModelAdmin):
     """
     작성자 : 최준영
-    내용 : 캠페인 admin 등록 클래스입니다.
-    list_display는 ManyToManyField를 지원하지 않습니다.
+    내용 : 캠페인 admin 페이지 등록 클래스입니다.
+    list_display는 ManyToManyField를 지원하지 않아 like는 넣지 않았습니다.
     최초 작성일 : 2023.06.06
     업데이트 일자 : 
     """
@@ -52,4 +52,51 @@ class ReviewDisplay(admin.ModelAdmin):
         'title',
         'user',
         'content',
+    ]
+
+
+@admin.register(Funding)
+class FundingDisplay(admin.ModelAdmin):
+    """
+    작성자 : 최준영
+    내용 : 펀딩 admin 페이지 등록 클래스입니다.
+    최초 작성일 : 2023.06.07
+    업데이트 일자 : 
+    """
+    list_display = [
+        'campaign',
+        'funding_startdate',
+        'deadline',
+        'goal',
+        'current',
+        'approvefile',
+        'created_at',
+        'updated_at',
+    ]
+    fields = [
+        'campaign',
+        'funding_startdate',
+        'deadline',
+        'goal',
+        'current',
+        'approvefile',
+    ]
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+    list_filter = [
+        'campaign',
+        'funding_startdate',
+        'deadline',
+    ]
+    search_fields = [
+        'campaign',
+        'funding_startdate',
+        'deadline',
+        'goal',
+        'current',
+        'approvefile',
+        'created_at',
+        'updated_at',
     ]
