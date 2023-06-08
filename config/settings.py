5ceb614c5d5d7cf6036e82ed1874e2007587d2e5
Park Jihong <elan5027@naver.com>
2023-06-08 09:18:49 +0900

    Feat : SQL 쿼리 확인을 위한 로깅 작성 ---본문--- - settings.py에 로깅 기능 추가
    --- 꼬리말 ---
    Feat #39

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,22 @@ CHANNEL_LAYERS = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
 
 DATABASES = {
     'dev': {
