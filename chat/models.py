9ed933e838c6c0f9ac5411e1d16c37ebe7345083
Park Jihong <elan5027@naver.com>
2023-06-15 03:53:23 +0900

    Update : 채팅의 기능의 변화로 인한 업데이트 ---본문--- - 1:1 채팅만 존재하여 모델의 간략화 - 기존의 동기기반에 일부 전송만 비동기 처리한 부분 비동기 기반으로 변경 - 모델의 간략화로 인한 View의 간략화 - 테스트 코드 전면 수정 --- 꼬리말 --- Update #111

--- chat/models.py
+++ chat/models.py
@@ -7,35 +7,23 @@ class Room(models.Model):
     작성자 : 박지홍
     내용 : 채팅을 위한 방을 만드는 모델
     최초 작성일 : 2023.06.06
-    업데이트 일자 :
+    업데이트 일자 : 2023.06.15
+    #06.15 : 
+        - 채팅방을 읽었는지 유무를 판단하기 위한 필드를 추가.
+        - 1:1 통신만 존재함으로 인해 기존의 구조를 한 단계 덜어냄.
     """
-
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    counselor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="counselor_user", db_column="counselor")
+    advisee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="advisee_user", db_column="advisee")
+    counselor_read = models.BooleanField(default=False)
+    advisee_read = models.BooleanField(default=False)
 
     class Meta:
         db_table = "room"
-
-
-class RoomJoin(models.Model):
-    """
-    작성자 : 박지홍
-    내용 : 채팅방 속에 존재할 유저를 만드는 모델
-    최초 작성일 : 2023.06.06
-    업데이트 일자 :
-    """
-
-    user_id = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="roomJoin", db_column="user_id"
-    )
-    room_id = models.ForeignKey(
-        Room, on_delete=models.CASCADE, related_name="roomJoin", db_column="room_id"
-    )
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "roomJoin"
 
 
 class Message(models.Model):
@@ -43,24 +31,22 @@ class Message(models.Model):
     작성자 : 박지홍
     내용 : 채팅방에 유저가 남기는 메세지를 보관할 모델
     최초 작성일 : 2023.06.06
-    업데이트 일자 :
+    업데이트 일자 : 2023.06.15
+    #06.15 : 1:1 통신만 존재함으로 인해 기존의 구조를 한 단계 덜어냄.
     """
 
     user_id = models.ForeignKey(
-        User, related_name="message", on_delete=models.CASCADE, db_column="user_id"
+        User, related_name="messages", on_delete=models.CASCADE, db_column="user_id"
     )
     room_id = models.ForeignKey(
-        Room, related_name="message", on_delete=models.CASCADE, db_column="room_id"
+        Room, related_name="messages", on_delete=models.CASCADE, db_column="room_id"
     )
     message = models.CharField(max_length=512)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = "message"
+        db_table = "messages"
 
     def __str__(self):
         return self.user_id.email
-
-    def last_30_messages(self, room_id):
-        return Message.objects.filter(room_id=room_id).order_by("created_at")[:30]
