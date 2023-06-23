198e752467e7377cfed7c62589b0d1a7031711c4
Soeun_Jang <azulcielo_@naver.com>
2023-06-24 02:39:34 +0900

    <Test> 상품 테스트코드 작성 (상품 조회,수정,삭제) 1. 관리자별 일반유저별 권한 테스트 2. 상품 상세 조회/수정/삭제 테스트 3. 모든 상품 내역 조회(쿼리 매개변수별, 검색어 별)
    --- 꼬리말 ---
     <Test> #77

--- shop/tests.py
+++ shop/tests.py
@@ -1,8 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from users.models import User
-from .models import ShopProduct, ShopCategory
-from faker import Faker
+from .models import ShopProduct, ShopCategory, ShopImageFile
 from datetime import timedelta
 from django.utils import timezone
 import random
@@ -29,9 +28,9 @@ class ProductPostTest(APITestCase):
     '''
     작성자: 장소은
     내용: 일반유저가 상품 post 했을 때, admin유저가 post했을 때 testcode 작성
-          다중이미지 업로드 기능 업데이트 
+          다중이미지 업로드 기능 업데이트
     작성일: 2023.06.07
-    업데이트: 2023.06.23  
+    업데이트: 2023.06.23
     '''
     @classmethod
     def setUpTestData(cls):
@@ -47,7 +46,6 @@ class ProductPostTest(APITestCase):
         cls.admin_data = {"email": "adminuser@test.com",
                           "password": "Xptmxm123@456"}
 
-        cls.faker = Faker()
         cls.category_data = {
             'category_name': "카테고리"
         }
@@ -89,7 +87,7 @@ class ProductPostTest(APITestCase):
         )
         self.assertEqual(response.status_code, 201)
 
-    def test_notadmin_post_product(self):
+    def test_not_admin_post_product(self):
         response = self.client.post(
             path=reverse("category_sortby_product_view", kwargs={
                          "category_id": self.category.id}),
@@ -97,11 +95,182 @@ class ProductPostTest(APITestCase):
             HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
         )
 
-        self.assertEquals(response.status_code, 403)
+        self.assertEqual(response.status_code, 403)
+
+
+class ProductListViewAPITest(APITestCase):
+    '''
+    작성자 : 장소은
+    내용 : 상품의 전체 목록 GET 요청, 쿼리매개변수 별 GET, 검색 GET요청에 대한 테스트
+    작성일 : 2023.06.23
+    '''
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse('product_sortby_view')
+
+        date = timezone.now() + timedelta(seconds=random.randint(0, 86400))
+
+        cls.category_data = {
+            'category_name': "카테고리"
+        }
+        cls.category = ShopCategory.objects.create(**cls.category_data)
+        cls.product_data = {
+            'product_name': "상품",
+            'product_desc': "테스트",
+            'category': cls.category,
+            'product_date': date,
+            'product_price': 10000,
+            'product_stock': 123,
+            "images": [
+                {
+                    "id": 1,
+                    "product": 1,
+                    "image_file": ""
+                }]
+        }
+
+        image_file = arbitrary_image()
+        cls.product_data["images"][0]["image_file"] = image_file
+
+    def test_product_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sort_by_hits(self):
+        response = self.client.get(self.url, {'sort_by': 'hits'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_query(self):
+        search_query = '에코백'
+        response = self.client.get(self.url, {'search_query': search_query})
+        self.assertEqual(response.status_code, 200)
 
 
 class ProductUpdateTest(APITestCase):
     '''
-    내용 : 상품 수정 
+    작성자 : 장소은
+    내용 : 상품 상세 조회, 수정, 삭제 테스트(유저 / 관리자)
+    작성일 : 2023.06.23
+    '''
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser(
+            email="adminuser@test.com", username="관리자소은", password="Xptmxm123@456")
+        cls.admin_data = {"email": "adminuser@test.com",
+                          "password": "Xptmxm123@456"}
+
+        date = timezone.now() + timedelta(seconds=random.randint(0, 86400))
+
+        cls.user_data = {
+            "email": "test@google.com",
+            "username": "testuser",
+            "password": "Xptmxm123@456"
+        }
+        cls.user = User.objects.create_user(**cls.user_data)
+
+        cls.category_data = {
+            'category_name': "카테고리"
+        }
+        cls.category = ShopCategory.objects.create(**cls.category_data)
+        cls.product_data = {
+            'product_name': "상품",
+            'product_desc': "테스트",
+            'category': cls.category,
+            'product_date': date,
+            'product_price': 10000,
+            'product_stock': 123
+        }
+        cls.product = ShopProduct.objects.create(
+            **cls.product_data)
+
+        image_data = {
+            "id": 1,
+            "product": cls.product,
+            "image_file": ""
+        }
+        cls.image = ShopImageFile.objects.create(**image_data)
+        cls.product.images.set([cls.image])
+        cls.product = ShopProduct.objects.create(
+            **cls.product_data)
+
+        cls.url = reverse('product_detail_view', kwargs={
+            "product_id": cls.product.id})
+        cls.updated_product_data = {
+            'product_name': "수정 상품",
+            'product_desc': "수정 테스트",
+            'product_price': 20000,
+            'product_stock': 50
+        }
+
+    def setUp(self):
+        self.access_token = self.client.post(
+            reverse("log_in"), self.user_data).data["access"]
+        self.admin_access_token = self.client.post(
+            reverse('log_in'), self.admin_data).data['access']
+
+    def test_user_product_put(self):
+        response = self.client.put(
+            self.url,
+            data=self.updated_product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_user_product_put(self):
+        response = self.client.put(
+            self.url,
+            data=self.updated_product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_access_token}",
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+class ProductDeleteTest(APITestCase):
+    '''
+    내용 : 상품 삭제
+    '''
+    pass
+
+
+class ProductRestockNotificatinonTest(APITestCase):
+    '''
+    내용 : 재입고 알림 신청
+    '''
+    pass
+
+
+class ProductOrderTest(APITestCase):
+    '''
+    내용 : 상품 주문
+    '''
+    pass
+
+
+class CategroyPostTest(APITestCase):
+    '''
+    내용 : 카테고리 생성
+    '''
+    pass
+
+
+class RestockNotificationSendTest(APITestCase):
+    '''
+    내용 : 재입고 알림 신청자들에게 알림 전송
+    '''
+    pass
+
+
+class CategoryProductListTest(APITestCase):
+    '''
+    내용 : 카테고리별 상품 목록 조회
+    '''
+    pass
+
+
+class MypageOrderListTest(APITestCase):
+    '''
+    내용 : 마이페이지 주문 내역 조회
     '''
     pass
