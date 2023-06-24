b97d4796fd50c7b08eabf172118be60963392b9b
Soeun_Jang <azulcielo_@naver.com>
2023-06-24 20:19:10 +0900

    <Test> <Fix> 마이페이지 캠페인 상세내역 조회 에러 수정, 테스트 코드 추가
    1. 캠페인 상세 내역 조회시 id값을 받기 위해 시리얼라이저 변경
    2. Shop 테스트코드 추가
    3. Shop 로그인 하지 않은 유저도 Detail 페이지 볼 수 있게 수정
    
     [Fix] [Test] #209

--- shop/tests.py
+++ shop/tests.py
@@ -1,12 +1,18 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from users.models import User
-from .models import ShopProduct, ShopCategory, ShopImageFile
+from .models import (
+    ShopProduct,
+    ShopCategory,
+    ShopImageFile,
+    RestockNotification
+)
 from datetime import timedelta
 from django.utils import timezone
 import random
 from PIL import Image
 from io import BytesIO
+from .signals import send_notifications
 
 
 def arbitrary_image():
@@ -211,6 +217,9 @@ class ProductUpdateTest(APITestCase):
             reverse('log_in'), self.admin_data).data['access']
 
     def test_user_product_put(self):
+        '''
+        권한이 없는 일반유저가 PUT할 때 테스트 
+        '''
         response = self.client.put(
             self.url,
             data=self.updated_product_data,
@@ -218,7 +227,10 @@ class ProductUpdateTest(APITestCase):
         )
         self.assertEqual(response.status_code, 403)
 
-    def test_user_product_put(self):
+    def test_admin_product_put(self):
+        '''
+        관리자가 PUT할 때 테스트 
+        '''
         response = self.client.put(
             self.url,
             data=self.updated_product_data,
@@ -226,51 +238,179 @@ class ProductUpdateTest(APITestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-
-class ProductDeleteTest(APITestCase):
-    '''
-    내용 : 상품 삭제
-    '''
-    pass
+    def test_admin_product_delete(self):
+        '''
+        관리자가 DELETE할 때 테스트 
+        '''
+        response = self.client.delete(
+            self.url,
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_access_token}",
+        )
+        self.assertEqual(response.status_code, 204)
 
 
 class ProductRestockNotificatinonTest(APITestCase):
     '''
     내용 : 재입고 알림 신청
     '''
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser(
+            email="adminuser@test.com", username="관리자소은", password="Xptmxm123@456")
+        cls.admin_data = {"email": "adminuser@test.com",
+                          "password": "Xptmxm123@456"}
+
+        date = timezone.now() + timedelta(seconds=random.randint(0, 86400))
+
+        cls.user_data = {
+            "email": "test@google.com",
+            "username": "testuser",
+            "password": "Xptmxm123@456"
+        }
+        cls.user = User.objects.create_user(**cls.user_data)
+
+        cls.category_data = {
+            'category_name': "카테고리"
+        }
+        cls.category = ShopCategory.objects.create(**cls.category_data)
+        cls.product_data = {
+            'product_name': "상품",
+            'product_desc': "테스트",
+            'category': cls.category,
+            'product_date': date,
+            'product_price': 10000,
+            'product_stock': 0,
+            'sold_out': True
+        }
+        cls.product = ShopProduct.objects.create(
+            **cls.product_data)
+
+        image_data = {
+            "id": 1,
+            "product": cls.product,
+            "image_file": ""
+        }
+        cls.image = ShopImageFile.objects.create(**image_data)
+        cls.product.images.set([cls.image])
+        cls.product = ShopProduct.objects.create(
+            **cls.product_data)
+
+        cls.url = reverse('restock_notification_view', kwargs={
+                          "product_id": cls.product.id})
+
+    def setUp(self):
+        self.access_token = self.client.post(
+            reverse("log_in"), self.user_data).data["access"]
+
+    def test_post_restock_notification(self):
+        '''
+        상품 품절 시 재입고 알림 POST 테스트
+        '''
+        response = self.client.post(
+            self.url,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_already_restock_notification(self):
+        '''
+        상품 재입고 알림을 이미 구독 중인 경우의 테스트 
+        '''
+        RestockNotification.objects.create(
+            product=self.product, user=self.user)
+
+        response = self.client.post(
+            self.url,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(
+            response.data['message'],
+            '이미 재입고 알림을 구독 하셨습니다.'
+        )
 
 
 class ProductOrderTest(APITestCase):
     '''
     내용 : 상품 주문
     '''
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        date = timezone.now() + timedelta(seconds=random.randint(0, 86400))
+
+        cls.user_data = {
+            "email": "test@google.com",
+            "username": "testuser",
+            "password": "Xptmxm123@456"
+        }
+        cls.user = User.objects.create_user(**cls.user_data)
+
+    def setUp(self):
+        self.access_token = self.client.post(
+            reverse("log_in"), self.user_data).data["access"]
+
+    def test_product_order(self):
+        '''
+        상품 주문을 위한 테스트
+        '''
+        response = self.client.post(
+            self.url,
+            data=self.product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 201)
 
 
-class CategroyPostTest(APITestCase):
-    '''
-    내용 : 카테고리 생성
-    '''
-    pass
+# class SendNotificationsTest(APITestCase):
+#     '''
+#     상품 재입고 시 알림 구독자들에게 알림 전송 테스트
+#     '''
+#     @classmethod
+#     def setUpTestData(cls):
+#         date = timezone.now() + timedelta(seconds=random.randint(0, 86400))
 
+#         cls.user_data = {
+#             "email": "test@google.com",
+#             "username": "testuser",
+#             "password": "Xptmxm123@456"
+#         }
+#         cls.user = User.objects.create_user(**cls.user_data)
 
-class RestockNotificationSendTest(APITestCase):
-    '''
-    내용 : 재입고 알림 신청자들에게 알림 전송
-    '''
-    pass
+#         cls.category_data = {
+#             'category_name': "카테고리"
+#         }
+#         cls.category = ShopCategory.objects.create(**cls.category_data)
+#         cls.product_data = {
+#             'product_name': "상품",
+#             'product_desc': "테스트",
+#             'restocked': True,
+#             'category': cls.category,
+#             'product_date': date,
+#             'product_price': 10000,
+#             'product_stock': 123,
+#             "images": [
+#                 {
+#                     "id": 1,
+#                     "product": 1,
+#                     "image_file": ""
+#                 }]
+#         }
 
+#         image_file = arbitrary_image()
+#         cls.product_data["images"][0]["image_file"] = image_file
 
-class CategoryProductListTest(APITestCase):
-    '''
-    내용 : 카테고리별 상품 목록 조회
-    '''
-    pass
+#     def test_send_notifications(self):
+#         notification = RestockNotification.objects.create(
+#             user=self.user, product=self.product)
+#         print(notification)
+#         send_notifications(sender=ShopProduct,
+#                            instance=self.product, created=False)
 
+#         notification.refresh_from_db()
+#         self.assertTrue(notification.notification_sent)
+        # self.assertEqual(notification.restock_message,
+        #                  f"상품 {self.product.product_name}이(가) 재입고되었습니다.")
 
-class MypageOrderListTest(APITestCase):
-    '''
-    내용 : 마이페이지 주문 내역 조회
-    '''
-    pass
+    # restock_created_at = models.DateTimeField(
+    #     auto_now_add=True)
+    # notification_sent = models.BooleanField(default=False)
+    # restock_message = models.CharField(max_length=255, null=True, blank=True)
