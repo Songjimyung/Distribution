35a6f5c4e377713e0fdff64b26951a7ccaf6c661
Soeun_Jang <azulcielo_@naver.com>
2023-06-25 20:08:43 +0900

    <Fix> 캠페인 상세 조회시 알림 메서드 삭제  #214

--- alarms/consumers.py
+++ alarms/consumers.py
@@ -28,36 +28,10 @@ class ChatNotificationConsumer(WebsocketConsumer):
         self.send(text_data=message)
 
 
-# class LoginConsumer(WebsocketConsumer):
-#     '''
-#     작성자 : 장소은
-#     내용 : 웹소켓 연결, notification_login_group 모든 컨슈머에게 메세지 보내기 
-#     최초 작성일: 2023.06.22
-#     '''
-
-#     def connect(self):
-#         # notification_login_group 그룹에 컨슈머 추가(알림 메세지 수신),
-#         async_to_sync(self.channel_layer.group_add)(
-#             "notification_login_group", self.channel_name)
-#         self.accept()  # 웹 소켓 연결 수락
-
-#     def disconnect(self, close_code):
-#         async_to_sync(self.channel_layer.group_discard)(
-#             "notification_login_group", self.channel_name)
-
-#     def receive(self, text_data):
-#         pass
-
-#     def notification_message(self, event):
-#         message = event['message']
-#         self.send(text_data=message)
-
-
-
 class NotificationConsumer(WebsocketConsumer):
     '''
     작성자 : 장소은
-    내용 : 웹소켓 연결, notification_group의 모든 컨슈머에게 메세지 보내기 
+    내용 : 웹소켓 연결, notification_group의 모든 컨슈머에게 메세지 보내기
     최초 작성일: 2023.06.21
     '''
 
@@ -77,3 +51,28 @@ class NotificationConsumer(WebsocketConsumer):
     def notification_message(self, event):
         message = event['message']
         self.send(text_data=message)
+
+
+# class LoginConsumer(WebsocketConsumer):
+#     '''
+#     작성자 : 장소은
+#     내용 : 웹소켓 연결, notification_login_group 모든 컨슈머에게 메세지 보내기
+#     최초 작성일: 2023.06.22
+#     '''
+
+#     def connect(self):
+#         # notification_login_group 그룹에 컨슈머 추가(알림 메세지 수신),
+#         async_to_sync(self.channel_layer.group_add)(
+#             "notification_login_group", self.channel_name)
+#         self.accept()  # 웹 소켓 연결 수락
+
+#     def disconnect(self, close_code):
+#         async_to_sync(self.channel_layer.group_discard)(
+#             "notification_login_group", self.channel_name)
+
+#     def receive(self, text_data):
+#         pass
+
+#     def notification_message(self, event):
+#         message = event['message']
+#         self.send(text_data=message)
